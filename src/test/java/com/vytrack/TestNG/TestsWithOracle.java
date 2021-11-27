620e248938e834aa2e8fe48791340e72fa6a9c02
OscarSah <oscar@cybertekschool.com>
2021-11-27 19:18:16 +0300

    before method to connect database

--- src/test/java/com/vytrack/TestNG/TestsWithOracle.java
+++ src/test/java/com/vytrack/TestNG/TestsWithOracle.java
@@ -1,15 +1,34 @@
 package com.vytrack.TestNG;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.sql.*;
 
 public class TestsWithOracle {
 
-@BeforeMethod
-public void setUp(){
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+    @BeforeMethod
+    public void setUp() throws SQLException {
+        String dbUrl = "jdbc:oracle:thin:@54.92.248.102:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+        String query = "select first_name,last_name, salary from employees";
+        connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(query);
+}
+    @AfterMethod
+    public void tearDown(){
 
 }
+    @Test
+    public void connectionTest(){
 
-
+    }
 
 
 
