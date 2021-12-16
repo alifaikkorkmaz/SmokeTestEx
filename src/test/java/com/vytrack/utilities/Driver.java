c29e747d0f62fd86cacb67d4872e77b03cae9a1f
OscarSah <oscar@cybertekschool.com>
2021-12-16 15:51:28 +0300

    Mobile Testing Part Added

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -1,5 +1,6 @@
 package com.vytrack.utilities;
 
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -70,6 +73,18 @@ public class Driver {
                     chromeOptions.setCapability("platform", Platform.ANY);
                     try {
                         driverPool.set(new RemoteWebDriver(new URL("http://18.212.6.39:4444/wd/hub"),chromeOptions));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                case "mobile_chrome":
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+                    desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.0");
+                    desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+                    desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.CHROME);
+                    desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                    try {
+                        driverPool.set(new RemoteWebDriver(new URL("http://localhost:4723/wd/hub"),desiredCapabilities));
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
